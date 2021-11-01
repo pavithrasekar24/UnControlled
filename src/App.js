1248b2de7e011c629d6996ab9381e9cf654e2678
pavithrasekar24 <spavithracse8@gmail.com>
2021-11-01 16:21:27 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -2,31 +2,31 @@ import React from 'react';
 import './style.css';
 import { useState, useRef } from 'react';
 
-function ControlledComponent() {
+function UnControlledComponent() {
   const _name = useRef(0);
   const [isShow, setIsShow] = useState(false);
   let handleClick = () => {
     console.log(_name, _name.current, _name.current.value);
-    setIsShow(true);
+    // setIsShow(true);
   };
   return (
     <div>
-      <h1> ControlledComponent</h1>
-      <p>using ref and useRef {isShow ? _name.current.value : ''}</p>
+      <h1> Un ControlledComponent</h1>
+      <p>using ref and useRef {_name.current.value}</p>
       <input type="text" ref={_name} />
       <button onClick={() => handleClick()}>Get</button>
     </div>
   );
 }
 
-function UnControlledComponent() {
+function ControlledComponent() {
   const [username, setUserName] = useState('');
   let handleChange = (e) => {
     setUserName(e.target.value);
   };
   return (
     <div>
-      <h1>UnControlledComponent</h1>
+      <h1>ControlledComponent</h1>
       <h2>Can't change if we using state</h2>
       <p>using setState {username}</p>
       <input type="text" value={username} onChange={(e) => handleChange(e)} />
